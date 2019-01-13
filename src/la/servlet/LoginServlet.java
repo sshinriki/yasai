1918806e7e8e204beb238b02393a4a7ea0cd0867
student <student@localhost>
2019-01-13 15:51:21 +0900

    15時51分

--- src/la/servlet/LoginServlet.java
+++ src/la/servlet/LoginServlet.java
@@ -1,41 +1,64 @@
 package la.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+public class LoginServlet extends HttpServlet{
+//テストのために（後で削除）
+	
+private static final String USER ="jack";
+private static final String PASS ="abc";
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+  response.setContentType("text/html;charset=UTF-8");
+  PrintWriter out = response.getWriter();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+  //actionリクエストパラメータの読み込み
+String action = request.getParameter("action");
+if (action.equals("login")){//ログイン時
+  String name = request.getParameter("name");
+  String passWord = request.getParameter("pw");
 
+  //ユーザ名とパスワードが一致したら
+  if (name.equals(USER) && passWord.equals(PASS)){
+    //セッション管理を行う
+    HttpSession session = request.getSession();
+    //ログイン済みの属性を設定する。
+    session.setAttribute("isLogin","true");
+    out.println("<html><head><title>ShowCart</title></head><body>");
+    out.println("<h1>ユーザ名またはパスワードが違います</h1>");
+    out.println("</body></html>");
+  }else{
+    out.println("<thml><head><title>ShowCart</title><head><body>");
+    out.println("<h1>ログアウトしました</h1>");
+    out.println("</body></html>");
+  }
+
+}else if(action.equals("logout")){//ログアウト時
+  //すでに作成されているセッション領域を取得する。新しくは作成しない
+  HttpSession session = request.getSession(false);
+  if(session !=null){
+    //セッション領域を無効にする。
+    session.invalidate();
+    out.println("<html><head><title>ShowCart</title></head><body>");
+    out.println("<h1>ログアウトしました</h1>");
+    out.println("</body></html>");
+  }
+
+}
+}
+
+
+
+protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+  doGet(request,response);
+}
 }
