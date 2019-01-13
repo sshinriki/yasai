eea7d95cf6cdb933a61eaca3d97945067208873e
student <student@localhost>
2019-01-13 10:35:49 +0900

    1035

---
+++ WebContent/itemList.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>商品一覧</title>
+</head>
+<body>
+
+<jsp:include page="/menu.jsp"/>
+<h3>商品一覧</h3>
+
+<c:forEach items="${items}" var="item">
+	<form action="/kaihatu/ShowItemServlet?action=list&code=${item.code}" method="post">
+
+		<input type="hidden" name="item_code" value="${item.code}">
+		<table border=1>
+			<tr>
+				<td width="200" >商品番号：<b>${item.code}</b><br>
+				商品名：<b>${item.name}</b><br><br><br>
+				</td>
+				<td width="150"><img src="img/${item.img}" width="60"><br>
+				<input type="submit" value="商品詳細">
+				</td>
+			</tr>
+		</table>
+	</form>
+</c:forEach>
+</body>
+</html>
---
+++ src/la/bean/ItemBean.java
@@ -0,0 +1,91 @@
+package la.bean;
+
+import java.io.Serializable;
+
+public class ItemBean implements Serializable{
+	private int code;
+	private String name;
+	private int price;
+	private String img;
+	private int stock;
+	private String detail;
+	private int quantity;
+	/**
+	 * @param code
+	 * @param name
+	 * @param price
+	 * @param img
+	 * @param stock
+	 * @param detail
+	 * @param quantity
+	 */
+//コンストラクタ
+//すべてのフィールド
+	public ItemBean(int code, String name, int price, String img, int stock,
+			String detail, int quantity) {
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		this.img = img;
+		this.stock = stock;
+		this.detail = detail;
+		this.quantity = quantity;
+	}
+//商品
+	public ItemBean(int code, String name, int price, String img, int stock,
+			String detail) {
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		this.img = img;
+		this.stock = stock;
+		this.detail = detail;
+	}
+	public ItemBean(){
+
+	}
+//セッターゲッター
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
+
